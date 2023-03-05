0d0144da927c05a1c346ea808e5685dca72225ec
joscord <josecorderodev@gmail.com>
2023-03-05 11:26:07 -0300

    feat: add useEffect logic to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import { useStatem, useEffect } from 'react'
 import './App.css'
+import ArticlesList from './components/stateful/ArticlesList'
 
 function App() {
-  const [bitcoinArticles, setBitcoinArticles] = useState(0)
+  const [bitcoinArticles, setBitcoinArticles] = useState([])
   const fetchData = async () => {
     const endpoint = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=594fd65e60414aeaaf3dc1690ef90a5d"
-    const response = axios.get(endpoint)
+    const response = await axios.get(endpoint)
     const { articles } = response.data 
     setBitcoinArticles(articles)
   }
 
+  useState(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
+      <ArticlesList articles={bitcoinArticles}/>
     </div>
   )
 }
