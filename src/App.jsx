61303c946fa00e7a2105c615f634dc945af78094
joscord <josecorderodev@gmail.com>
2023-03-05 11:21:22 -0300

    feat: create bitcoin articles state and install axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bitcoinArticles, setBitcoinArticles] = useState(0)
+  const fetchData = async () => {
+    const endpoint = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=594fd65e60414aeaaf3dc1690ef90a5d"
+    const response = axios.get(endpoint)
+    const { articles } = response.data 
+    setBitcoinArticles(articles)
+  }
 
   return (
     <div className="App">
